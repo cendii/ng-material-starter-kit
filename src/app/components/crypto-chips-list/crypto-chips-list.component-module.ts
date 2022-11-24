e12f885215307e7fa69adb74dad755b95ac1bdd9
katarzynacendlak <kasia.cendlak@gmail.com>
2022-11-25 00:30:10 +0100

    feat: lesson 4, crypto chips component

---
+++ src/app/components/crypto-chips-list/crypto-chips-list.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CryptoChipsListComponent } from './crypto-chips-list.component';
+
+@NgModule({
+  imports: [],
+  declarations: [CryptoChipsListComponent],
+  providers: [],
+  exports: [CryptoChipsListComponent]
+})
+export class CryptoChipsListComponentModule {
+}
