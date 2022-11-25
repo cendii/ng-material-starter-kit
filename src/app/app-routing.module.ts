4b6db5ac1df882775ce368ad9e08915175c1135a
katarzynacendlak <kasia.cendlak@gmail.com>
2022-11-25 01:21:20 +0100

    feat: lesson 4, public holidays select template, path in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,26 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { CryptoChipsListComponent } from './components/crypto-chips-list/crypto-chips-list.component';
+import { PublicHolidaysSelectComponent } from './components/public-holidays-select/public-holidays-select.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { CryptoChipsListComponentModule } from './components/crypto-chips-list/crypto-chips-list.component-module';
 import { CryptoChipsServiceModule } from './services/crypto-chips.service-module';
+import { PublicHolidaysSelectComponentModule } from './components/public-holidays-select/public-holidays-select.component-module';
+import { PublicHolidaysServiceModule } from './services/public-holidays.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'products', component: ProductListComponent },
     { path: 'categories', component: CategoriesListComponent },
-    { path: 'crypto', component: CryptoChipsListComponent }
+    { path: 'crypto', component: CryptoChipsListComponent },
+    { path: 'public-holidays', component: PublicHolidaysSelectComponent }
   ]), ProductListComponentModule, ProductsServiceModule,
     CategoriesListComponentModule, CategoriesServiceModule,
-    CryptoChipsListComponentModule, CryptoChipsServiceModule],
+    CryptoChipsListComponentModule, CryptoChipsServiceModule,
+    PublicHolidaysSelectComponentModule, PublicHolidaysServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/public-holidays-select/public-holidays-select.component-module.ts
+++ src/app/components/public-holidays-select/public-holidays-select.component-module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 import { PublicHolidaysSelectComponent } from './public-holidays-select.component';
 
 @NgModule({
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatSelectModule, MatOptionModule, CommonModule],
   declarations: [PublicHolidaysSelectComponent],
   providers: [],
   exports: [PublicHolidaysSelectComponent]
